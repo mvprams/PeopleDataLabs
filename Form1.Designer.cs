b2d10d6c57c3b365e4b4290c33922a5369943de2
Andrew Winter <winterad@gib.cc>
2024-04-14 16:15:34 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PeopleDataLabs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Fname_tb = new System.Windows.Forms.TextBox();
+            this.Fname_lbl = new System.Windows.Forms.Label();
+            this.Lname_lbl = new System.Windows.Forms.Label();
+            this.Lname_tb = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Fname_tb
+            // 
+            this.Fname_tb.Location = new System.Drawing.Point(106, 60);
+            this.Fname_tb.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Fname_tb.Name = "Fname_tb";
+            this.Fname_tb.Size = new System.Drawing.Size(171, 20);
+            this.Fname_tb.TabIndex = 0;
+            // 
+            // Fname_lbl
+            // 
+            this.Fname_lbl.AutoSize = true;
+            this.Fname_lbl.Location = new System.Drawing.Point(39, 60);
+            this.Fname_lbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Fname_lbl.Name = "Fname_lbl";
+            this.Fname_lbl.Size = new System.Drawing.Size(60, 13);
+            this.Fname_lbl.TabIndex = 1;
+            this.Fname_lbl.Text = "First Name:";
+            // 
+            // Lname_lbl
+            // 
+            this.Lname_lbl.AutoSize = true;
+            this.Lname_lbl.Location = new System.Drawing.Point(39, 109);
+            this.Lname_lbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Lname_lbl.Name = "Lname_lbl";
+            this.Lname_lbl.Size = new System.Drawing.Size(61, 13);
+            this.Lname_lbl.TabIndex = 3;
+            this.Lname_lbl.Text = "Last Name:";
+            // 
+            // Lname_tb
+            // 
+            this.Lname_tb.Location = new System.Drawing.Point(106, 109);
+            this.Lname_tb.Margin = new System.Windows.Forms.Padding(2);
+            this.Lname_tb.Name = "Lname_tb";
+            this.Lname_tb.Size = new System.Drawing.Size(171, 20);
+            this.Lname_tb.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 411);
+            this.Controls.Add(this.Lname_lbl);
+            this.Controls.Add(this.Lname_tb);
+            this.Controls.Add(this.Fname_lbl);
+            this.Controls.Add(this.Fname_tb);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Fname_tb;
+        private System.Windows.Forms.Label Fname_lbl;
+        private System.Windows.Forms.Label Lname_lbl;
+        private System.Windows.Forms.TextBox Lname_tb;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PeopleDataLabs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+            private const string API_KEY = "YOUR API KEY";
+        private const string PDL_URL = "https://api.peopledatalabs.com/v5/person/bulk";
+        private const string CSV_FILENAME = "enriched_profiles.csv";
+
+        static async Task Main(string[] args)
+        {
+            var allRecords = new List<Dictionary<string, string>>();
+
+            using (var reader = new StreamReader("profiles.csv"))
+            {
+                var profileCount = 0;
+                while (!reader.EndOfStream)
+                {
+                    var line = await reader.ReadLineAsync();
+                    if (profileCount > 0)
+                    {
+                        var profile = new Dictionary<string, string>
+                        {
+                            { "profile", line }
+                        };
+                        allRecords.Add(profile);
+                    }
+                    profileCount++;
+                }
+                Console.WriteLine($"Read {profileCount - 1} profiles.");
+            }
+
+            var data = new Dictionary<string, List<Dictionary<string, string>>>
+            {
+                { "requests", allRecords }
+            };
+
+            var json = JsonConvert.SerializeObject(data);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-api-key", API_KEY);
+                var response = await client.PostAsync(PDL_URL, content);
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var jsonResponses = JsonConvert.DeserializeObject<List<JObject>>(responseContent);
+
+                foreach (var jsonResponse in jsonResponses)
+                {
+                    if (jsonResponse["status"].Value<int>() == 200)
+                    {
+                        var record = jsonResponse["data"].ToObject<Dictionary<string, string>>();
+                        allRecords.Add(record);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Bulk Person Enrichment Error: {jsonResponse}");
+                    }
+                }
+            }
+
+            SaveProfilesToCsv(allRecords);
+        }
+
+        static void SaveProfilesToCsv(List<Dictionary<string, string>> profiles)
+        {
+            var csvHeaderFields = new List<string>
+            {
+                "full_name", "job_title", "job_company_name", "job_company_website",
+                "work_email", "mobile_phone", "linkedin_url"
+            };
+
+            using (var writer = new StreamWriter(CSV_FILENAME))
+            {
+                writer.WriteLine(string.Join(",", csvHeaderFields));
+
+                foreach (var profile in profiles)
+                {
+                    var csvLine = new List<string>();
+                    foreach (var field in csvHeaderFields)
+                    {
+                        csvLine.Add(profile.ContainsKey(field) ? profile[field] : "");
+                    }
+                    writer.WriteLine(string.Join(",", csvLine));
+                }
+                Console.WriteLine($"Wrote {profiles.Count} lines to: '{CSV_FILENAME}'.");
+            }
+        
+        }
+    }
+}
